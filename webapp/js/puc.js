6087f5bdcc741d91e4ae0aa028d3ea1ebad0b253
agrohe21 <agrohe21@e74b1358-397f-11de-b310-9106b1f7c239>
2010-08-06 21:50:22 +0000

    made changes to implement xmql plugin.

--- webapp/js/puc.js
+++ webapp/js/puc.js
@@ -6,15 +6,15 @@ if (!pentaho.PUC) {
 		enabled: (window.parent != null && window.parent.mantle_initialized == true),
 		refreshBrowsePanel: function(){
 			// if possible refresh the solution browser panel
-			if (Pentaho.PUC.enabled && window.parent.mantle_refreshRepository) {
+			if (pentaho.PUC.enabled && window.parent.mantle_refreshRepository) {
 				window.parent.mantle_refreshRepository();
 			}
 		},
 		toggleEditButton: function(lower){
 			// if possible, lower/depress the 'Edit' toolbar button
-			if (Pentaho.PUC.enabled && window.parent.setContentEditSelected) {
+			if (pentaho.PUC.enabled && window.parent.setContentEditSelected) {
 				window.parent.setContentEditSelected((lower == true) ? true : false);
 			}
 		}
 	}
-} // if Pentaho.PUC
+} // if pentaho.PUC
