e60a449192d8e8484cc642b60334d28063e83e6c
agrohe21 <agrohe21@e74b1358-397f-11de-b310-9106b1f7c239>
2010-10-05 18:46:13 +0000

    initial entry

---
+++ webapp/js/oop.js
@@ -0,0 +1,11 @@
+        function object(o){
+            function F(){}
+            F.prototype = o;
+            return new F();
+        }
+            
+        function inheritPrototype(subType, superType){
+            var prototype = object(superType.prototype);   //create object
+            prototype.constructor = subType;               //augment object
+            subType.prototype = prototype;                 //assign object
+        }
